45dd15a12bb72ce43abc1f23abd0cb108e63d898
Danny <lannyducas@gmail.com>
2022-07-25 15:40:17 -0400

    commit tree 2aa86011ae179f1c2086cdda26c7607c20fb9244

--- src/index.js
+++ src/index.js
@@ -3,3 +3,7 @@ const init = () => {
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+fetch ("http://localhost:3000/movies")
+    .then((res) => res.json())
+    .then((json) => console.log(json));
